b6613877c9c5d256f60ae431c8729c39f52812f2
Adrien Fauquembergue <adrien_fauque@hotmail.fr>
2024-04-30 18:00:22 +0200

    database.php file implementation

--- app/core/Database.php
+++ app/core/Database.php
@@ -1,6 +1,21 @@
 <?php
+
+namespace App\Core;
+
 require_once __DIR__ . '/../config/config.php';
 
-class Database
-{
+// On "importe" PDO
+use PDO;
+use PDOException;
+
+class Database extends PDO {
+
+    // Instance unique de la classe
+    private static $instance = null;
+
+    private function __construct() 
+    {
+        // DSN de connexion
+        $dsn = $dsn = "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4";
+    }
 }
