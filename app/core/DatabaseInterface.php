6b3e25ce03d289531d11d788ac784c494b36c6fd
Adrien Fauquembergue <adrien_fauque@hotmail.fr>
2024-05-24 14:19:51 +0200

    correction DatabaseInterface.php style code

--- app/core/DatabaseInterface.php
+++ app/core/DatabaseInterface.php
@@ -7,19 +7,21 @@ use Iterator;
 
 /**
  * Interface DatabaseInterface
- * 
+ *
  * Cette interface définit les méthodes standard pour interagir avec une base de données.
  * Les classes qui implémentent cette interface doivent fournir des implémentations
  * spécifiques pour les méthodes de manipulation de la base de données.
  */
 interface DatabaseInterface
 {
+
+
     /**
      * Exécute une requête SQL et retourne les résultats.
      * Peut lever une exception si la requête échoue.
      *
-     * @param string $sql La chaîne de la requête SQL à exécuter.
-     * @param array $params Les paramètres à lier à la requête, si nécessaire.
+     * @param string $sql    La chaîne de la requête SQL à exécuter.
+     * @param array $params  Les paramètres à lier à la requête, si nécessaire.
      * @return Iterator Les résultats de la requête.
      */
     public function query(string $sql, array $params = []): Iterator;
@@ -28,13 +30,22 @@ interface DatabaseInterface
      * Prépare une requête SQL avec des paramètres et retourne un objet pour exécuter cette requête.
      * Peut lever une exception si la préparation échoue.
      *
-     * @param string $sql La chaîne de la requête SQL à préparer.
-     * @param array $params Les paramètres à lier à la requête préparée.
+     * @param string $sql    La chaîne de la requête SQL à préparer.
+     * @param array $params  Les paramètres à lier à la requête préparée.
      * @return PDOStatement Un objet représentant la requête préparée.
      */
     public function prepare(string $sql): \PDOStatement;
 
+
+    /**
+     * Exécute une déclaration PDO avec les paramètres donnés.
+     *
+     * @param \PDOStatement $stmt   La déclaration PDO à exécuter.
+     * @param array         $params Les paramètres à lier à la déclaration.
+     * @return bool         Retourne true si l'exécution a réussi, sinon false.
+     */
     public function execute(\PDOStatement $stmt, array $params = []): bool;
+
 
     /**
      * Retourne l'identifiant de la dernière ligne insérée ou la valeur d'une séquence.
@@ -43,4 +54,5 @@ interface DatabaseInterface
      * @return string L'identifiant de la dernière ligne insérée sous forme de chaîne.
      */
     public function lastInsertId(): string;
+
 }
