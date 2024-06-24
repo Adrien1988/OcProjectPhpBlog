117dab3c865eb51580dc3bdc5b41a718bd2c2cc7
Adrien Fauquembergue <adrien_fauque@hotmail.fr>
2024-06-24 12:06:41 +0200

    modification of the CI file to take into account JS files.

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    extends: [
+      'airbnb-base',
+      'prettier',
+    ],
+    plugins: ['prettier'],
+    rules: {
+      'prettier/prettier': 'error',
+    },
+    env: {
+      browser: true,
+      node: true,
+    },
+  };
+  
