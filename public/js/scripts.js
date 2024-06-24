272972b0900729c4947700709ad3abcc0c1e1e99
Adrien Fauquembergue <adrien_fauque@hotmail.fr>
2024-06-24 14:35:34 +0200

    fix code style

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,50 +7,49 @@
 /* global bootstrap */
 
 const handleDOMContentLoaded = () => {
-    // Navbar shrink function.
-    const navbarShrink = () => {
-      const navbarCollapsible = document.body.querySelector('#mainNav');
-      if (!navbarCollapsible) {
-        return;
-      }
-      if (window.scrollY === 0) {
-        navbarCollapsible.classList.remove('navbar-shrink');
-      } else {
-        navbarCollapsible.classList.add('navbar-shrink');
-      }
-    };
-  
-    // Shrink the navbar.
-    navbarShrink();
-  
-    // Shrink the navbar when page is scrolled.
-    document.addEventListener('scroll', navbarShrink);
-  
-    // Activate Bootstrap scrollspy on the main nav element.
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-      // Initialise ScrollSpy
-      // eslint-disable-next-line no-unused-vars
-      const scrollSpy = new bootstrap.ScrollSpy(document.body, {
-        target: '#mainNav',
-        rootMargin: '0px 0px -40%',
-      });
+  // Navbar shrink function.
+  const navbarShrink = () => {
+    const navbarCollapsible = document.body.querySelector('#mainNav');
+    if (!navbarCollapsible) {
+      return;
     }
-  
-    // Collapse responsive navbar when toggler is visible.
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-      document.querySelectorAll('#navbarResponsive .nav-link'),
-    );
-  
-    responsiveNavItems.forEach((responsiveNavItem) => {
-      responsiveNavItem.addEventListener('click', () => {
-        if (window.getComputedStyle(navbarToggler).display !== 'none') {
-          navbarToggler.click();
-        }
-      });
-    });
+    if (window.scrollY === 0) {
+      navbarCollapsible.classList.remove('navbar-shrink');
+    } else {
+      navbarCollapsible.classList.add('navbar-shrink');
+    }
   };
-  
-  window.addEventListener('DOMContentLoaded', handleDOMContentLoaded);
-  
+
+  // Shrink the navbar.
+  navbarShrink();
+
+  // Shrink the navbar when page is scrolled.
+  document.addEventListener('scroll', navbarShrink);
+
+  // Activate Bootstrap scrollspy on the main nav element.
+  const mainNav = document.body.querySelector('#mainNav');
+  if (mainNav) {
+    // Initialise ScrollSpy
+    // eslint-disable-next-line no-unused-vars
+    const scrollSpy = new bootstrap.ScrollSpy(document.body, {
+      target: '#mainNav',
+      rootMargin: '0px 0px -40%',
+    });
+  }
+
+  // Collapse responsive navbar when toggler is visible.
+  const navbarToggler = document.body.querySelector('.navbar-toggler');
+  const responsiveNavItems = [].slice.call(
+    document.querySelectorAll('#navbarResponsive .nav-link'),
+  );
+
+  responsiveNavItems.forEach((responsiveNavItem) => {
+    responsiveNavItem.addEventListener('click', () => {
+      if (window.getComputedStyle(navbarToggler).display !== 'none') {
+        navbarToggler.click();
+      }
+    });
+  });
+};
+
+window.addEventListener('DOMContentLoaded', handleDOMContentLoaded);
