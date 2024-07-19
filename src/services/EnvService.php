3cb689f4626aee4443acd0510096885bc82cc5db
Adrien Fauquembergue <adrien_fauque@hotmail.fr>
2024-07-19 15:10:03 +0200

    fix code style

--- src/services/EnvService.php
+++ src/services/EnvService.php
@@ -9,12 +9,14 @@ use Dotenv\Dotenv;
  */
 class EnvService
 {
+
     /**
      * Instance de Dotenv pour gérer les variables d'environnement.
      *
      * @var Dotenv
      */
     private Dotenv $dotenv;
+
 
     /**
      * Constructeur de la classe.
@@ -24,7 +26,9 @@ class EnvService
     public function __construct(Dotenv $dotenv)
     {
         $this->dotenv = $dotenv;
-    }
+
+    }//end __construct()
+
 
     /**
      * Charge les variables d'environnement à partir du fichier .env.
@@ -34,5 +38,8 @@ class EnvService
     public function loadEnv(): void
     {
         $this->dotenv->load();
-    }
-}
+
+    }//end loadEnv()
+
+
+}//end class
