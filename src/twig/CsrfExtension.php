c0917d756d9a1c5a6cd2f9663d9a10669fb4cbad
Adrien Fauquembergue <adrien_fauque@hotmail.fr>
2024-07-26 09:18:28 +0200

    fix code style in CsrfExtension.php file

--- src/twig/CsrfExtension.php
+++ src/twig/CsrfExtension.php
@@ -9,9 +9,23 @@ use App\Services\CsrfService;
 class CsrfExtension extends AbstractExtension
 {
 
+    /**
+     * Service CSRF.
+     *
+     * Le service utilisé pour gérer et valider les jetons CSRF.
+     *
+     * @var CsrfService
+     */
     private $csrfService;
 
 
+    /**
+     * Constructeur de la classe.
+     *
+     * Initialise le service CSRF.
+     *
+     * @param CsrfService $csrfService Le service CSRF pour la validation des jetons CSRF.
+     */
     public function __construct(CsrfService $csrfService)
     {
         $this->csrfService = $csrfService;
@@ -19,6 +33,13 @@ class CsrfExtension extends AbstractExtension
     }//end __construct()
 
 
+    /**
+     * Retourne les fonctions Twig personnalisées.
+     *
+     * Cette méthode enregistre une nouvelle fonction Twig appelée 'csrf_token' qui appelle la méthode 'getCsrfToken'.
+     *
+     * @return array Les fonctions Twig personnalisées.
+     */
     public function getFunctions()
     {
         return [
@@ -28,6 +49,13 @@ class CsrfExtension extends AbstractExtension
     }//end getFunctions()
 
 
+    /**
+     * Génère et retourne un jeton CSRF.
+     *
+     * @param string $tokenId L'identifiant du jeton CSRF.
+     *
+     * @return string La valeur du jeton CSRF généré.
+     */
     public function getCsrfToken(string $tokenId): string
     {
         return $this->csrfService->generateToken($tokenId);
