740666ff32c4949331a02d1146d59049527b5f3e
Adrien Fauquembergue <adrien_fauque@hotmail.fr>
2024-08-26 10:33:01 +0200

    fix eslint in scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,98 +7,106 @@
 /* global bootstrap */
 
 const handleDOMContentLoaded = () => {
-    // Navbar shrink function.
-    const navbarShrink = () => {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink');
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink');
-        }
-    };
-
-    // Shrink the navbar.
-    navbarShrink();
-
-    // Shrink the navbar when page is scrolled.
-    document.addEventListener('scroll', navbarShrink);
-
-    // Activate Bootstrap scrollspy on the main nav element.
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        // Initialise ScrollSpy.
-        // eslint-disable-next-line no-new
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            rootMargin: '0px 0px -40%',
-        });
+  // Navbar shrink function.
+  const navbarShrink = () => {
+    const navbarCollapsible = document.body.querySelector('#mainNav');
+    if (!navbarCollapsible) {
+      return;
     }
+    if (window.scrollY === 0) {
+      navbarCollapsible.classList.remove('navbar-shrink');
+    } else {
+      navbarCollapsible.classList.add('navbar-shrink');
+    }
+  };
 
-    // Collapse responsive navbar when toggler is visible.
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link'),
-    );
+  // Shrink the navbar.
+  navbarShrink();
 
-    responsiveNavItems.forEach((responsiveNavItem) => {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
+  // Shrink the navbar when page is scrolled.
+  document.addEventListener('scroll', navbarShrink);
+
+  // Activate Bootstrap scrollspy on the main nav element.
+  const mainNav = document.body.querySelector('#mainNav');
+  if (mainNav) {
+    // Initialise ScrollSpy.
+    // eslint-disable-next-line no-new
+    new bootstrap.ScrollSpy(document.body, {
+      target: '#mainNav',
+      rootMargin: '0px 0px -40%',
     });
+  }
 
-    // Function to reset the form.
-    const resetForm = () => {
-        const form = document.getElementById('contactForm');
-        if (form) {
-            form.reset();
-        }
-    };
+  // Collapse responsive navbar when toggler is visible.
+  const navbarToggler = document.body.querySelector('.navbar-toggler');
+  const responsiveNavItems = [].slice.call(
+    document.querySelectorAll('#navbarResponsive .nav-link'),
+  );
 
-    // Handle contact form submission with AJAX.
-    const submitButton = document.getElementById('submitButton');
-    if (submitButton) {
-        submitButton.addEventListener('click', function (event) {
-            event.preventDefault();
+  responsiveNavItems.forEach((responsiveNavItem) => {
+    responsiveNavItem.addEventListener('click', () => {
+      if (window.getComputedStyle(navbarToggler).display !== 'none') {
+        navbarToggler.click();
+      }
+    });
+  });
 
-            const form = document.getElementById('contactForm');
-            if (form) {
-                const formData = new FormData(form);
-
-                fetch(form.action, {
-                    method: 'POST',
-                    body: formData,
-                })
-                    .then(response => {
-                        if (!response.ok) {
-                            return response.text().then(text => { throw new Error(text); });
-                        }
-                        return response.text();
-                    })
-                    .then(data => {
-                        document.getElementById('modalMessage').innerHTML = data;
-                        const feedbackModal = new bootstrap.Modal(document.getElementById('feedbackModal'));
-                        feedbackModal.show();
-
-                        // Reset the form after submission.
-                        resetForm();
-
-                        // Add an event listener to reset the form when the modal is closed.
-                        const feedbackModalElement = document.getElementById('feedbackModal');
-                        feedbackModalElement.addEventListener('hidden.bs.modal', resetForm);
-                    })
-                    .catch(error => {
-                        document.getElementById('modalMessage').innerHTML = 'Erreur lors de l\'envoi du message: ' + error.message;
-                        const feedbackModal = new bootstrap.Modal(document.getElementById('feedbackModal'));
-                        feedbackModal.show();
-                    });
-            }
-        });
+  // Function to reset the form.
+  const resetForm = () => {
+    const form = document.getElementById('contactForm');
+    if (form) {
+      form.reset();
     }
+  };
+
+  // Handle contact form submission with AJAX.
+  const submitButton = document.getElementById('submitButton');
+  if (submitButton) {
+    submitButton.addEventListener('click', function (event) {
+      event.preventDefault();
+
+      const form = document.getElementById('contactForm');
+      if (form) {
+        const formData = new FormData(form);
+
+        fetch(form.action, {
+          method: 'POST',
+          body: formData,
+        })
+          .then((response) => {
+            if (!response.ok) {
+              return response.text().then((text) => {
+                throw new Error(text);
+              });
+            }
+            return response.text();
+          })
+          .then((data) => {
+            document.getElementById('modalMessage').innerHTML = data;
+            const feedbackModal = new bootstrap.Modal(
+              document.getElementById('feedbackModal'),
+            );
+            feedbackModal.show();
+
+            // Reset the form after submission.
+            resetForm();
+
+            // Add an event listener to reset the form when the modal is closed.
+            const feedbackModalElement =
+              document.getElementById('feedbackModal');
+            feedbackModalElement.addEventListener('hidden.bs.modal', resetForm);
+          })
+          .catch((error) => {
+            document.getElementById('modalMessage').innerHTML =
+              "Erreur lors de l'envoi du message: " + error.message;
+            const feedbackModal = new bootstrap.Modal(
+              document.getElementById('feedbackModal'),
+            );
+            feedbackModal.show();
+          });
+      }
+    });
+  }
 };
 
 window.addEventListener('DOMContentLoaded', handleDOMContentLoaded);
